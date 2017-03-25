f5b89241bff7aeb2ef4ca39e753ecbed668f4b5a
survivechris <survival_of_chris@hotmail.com>
2017-03-25 17:59:47 +0800

    restrict user actions from UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= (User.find(session[:user_id]) if session[:user_id])
+  end
+
+  def logged_in?
+    # it is the same as !!current_user
+    !current_user.nil?
+  end
+
+  def require_user
+    unless logged_in?
+      flash[:danger] = 'You must log in to perform this action!'
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def home
+    # can not see the home page (signup) if the user has logged in.
+    redirect_to articles_path if logged_in?
   end
 
   def about

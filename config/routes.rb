c1568966dcb048cfdf0d0e8a1a9a9367c0818c53
survivechris <survival_of_chris@hotmail.com>
2017-03-25 15:21:50 +0800

    add seesion controller for user login

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
 
+  # user login
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end

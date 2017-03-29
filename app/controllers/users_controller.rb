2e505c50dee6948f277ffd97773742478c5d8883
survivechris <survival_of_chris@hotmail.com>
2017-03-29 15:55:43 +0800

    login upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
     @user = User.new(user_params)
-
     if @user.save
       # use flash to pass some message to the very next action
-      flash[:success] = 'Welcome to Cycle around Taiwan!'
+      flash[:success] = "Welcome to Cycle around Taiwan! #{@user.username}"
+      # login upon signup
+      session[:user_id] = @user.id
       # after saving the user obj to DB
       # it would render someting to users
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end

2f5a05394f8e99441fb40a509db3b1545bf0543b
survivechris <survival_of_chris@hotmail.com>
2017-04-11 20:43:39 +0800

    add article and category association and the join table

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ActiveRecord::Base
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   validates :name, presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end

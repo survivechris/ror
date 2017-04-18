954278d54d2af1b017ce94644de2c050523effd4
survivechris <survival_of_chris@hotmail.com>
2017-04-18 21:43:16 +0800

    bugfix and allow admin to edit category name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,23 @@ class CategoriesController < ApplicationController
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      # use flash to pass some message to the very next action
+      flash[:success] = 'the category is successfully editted'
+      # after saving the category obj to DB
+      # it would render someting to users
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params
